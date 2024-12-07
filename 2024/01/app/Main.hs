67a1111938a5b8eb210f3d44a395c2e30f0ab500
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-12-07 15:00:20 +0900

    1

--- 2024/01/app/Main.hs
+++ 2024/01/app/Main.hs
@@ -1,4 +1,33 @@
 module Main where
 
+import Data.Functor ((<&>))
+import Data.List (sort)
+import System.Environment (getArgs)
+
+splitBy :: Char -> String -> [String]
+splitBy d = go ""
+ where
+  go :: String -> String -> [String]
+  go acc [x] = if x == d then [acc] else [acc <> [x]]
+  go acc (x : xs) = if x == d then [acc] <> go "" xs else go (acc <> [x]) xs
+  go _ "" = []
+
+getFilePath :: IO FilePath
+getFilePath = getArgs <&> head
+
+readInput :: FilePath -> IO [(Int, Int)]
+readInput f = do
+  content <- readFile f
+  let rows = map (splitBy ' ') (splitBy '\n' content)
+  return $ map (\l -> (read $ head l, read $ l !! 1)) rows
+
+solve :: [(Int, Int)] -> Int
+solve ps =
+  let a = sort (map fst ps)
+      b = sort (map snd ps)
+   in sum $ zipWith (\x y -> abs (x - y)) a b
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  source <- getFilePath >>= readInput
+  print $ solve source
