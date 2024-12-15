ed4f1c6cd706cc42d4364890fc2e2154807018a0
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-12-15 09:51:37 +0900

    solved 02

--- 2024/02/app/Main.hs
+++ 2024/02/app/Main.hs
@@ -14,23 +14,20 @@ splitBy d = go ""
 getFilePath :: IO FilePath
 getFilePath = getArgs <&> head
 
-type Source = [(Int, Int, Int, Int, Int)]
+type Source = [[Int]]
 type Answer = Int
 
 readInput :: FilePath -> IO Source
 readInput f = do
   content <- readFile f
   let rows = splitBy '\n' content
-  return $ map ((\l -> (read $ head l, read $ l !! 1, read $ l !! 2, read $ l !! 3, read $ l !! 4)) . splitBy ' ') rows
+  return $ map (map read . splitBy ' ') rows
 
 rule2 :: Int -> Int -> Bool
 rule2 a b = let x = abs (a - b) in x >= 1 && x <= 3
 
-tplToList :: (Int, Int, Int, Int, Int) -> [Int]
-tplToList (a, b, c, d, e) = [a, b, c, d, e]
-
 solve :: Source -> Answer
-solve source = sum $ map (calc . tplToList) source
+solve source = sum $ map calc source
  where
   calc :: [Int] -> Answer
   calc xs = go (compare (head xs) (xs !! 1)) xs
@@ -41,8 +38,11 @@ solve source = sum $ map (calc . tplToList) source
   go o [a, b] = if compare a b == o && rule2 a b then 1 else 0
   go o (a : b : rest) = if compare a b == o && rule2 a b then go o (b : rest) else 0
 
-makePairs :: (Int, Int, Int, Int, Int) -> [(Int, Int)]
-makePairs (a, b, c, d, e) = [(a, b), (b, c), (c, d), (d, e)]
+makePairs :: [Int] -> [(Int, Int)]
+makePairs [x, y] = [(x,y)]
+makePairs (x:y:rest) = (x,y) : makePairs (y : rest)
+makePairs [_] = []
+makePairs [] = []
 
 solve2 :: Source -> Answer
 solve2 source = sum (map (calc True . makePairs) source)
