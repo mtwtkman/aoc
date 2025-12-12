a3ea8fd89103005b73a557ec9f689cb45c1d15e0
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2025-12-12 17:35:08 +0900

    solved day4

--- 2025/haskell/04.hs
+++ 2025/haskell/04.hs
@@ -1,5 +1,8 @@
 module Main04 where
 
+-- I should make a map which has keys of positions and values of character..
+-- Current implementation is terribly inefficient by naive iteration.
+
 findItem :: (Int, Int) -> [String] -> Char
 findItem (x, y) rows = rows !! y !! x
 
@@ -16,6 +19,11 @@ pos (x, y) l1 l2
   | x == l1 = RightSide
   | y == l2 = Bottom
   | otherwise = Inside
+
+addUniquely :: (Eq a) => a -> [a] -> [a]
+addUniquely e xs
+  | e `elem` xs = xs
+  | otherwise = e : xs
 
 moveUpper :: (Int, Int) -> (Int, Int)
 moveUpper (x, y) = (x, y - 1)
@@ -34,11 +42,13 @@ moveBottomLeft = moveBottom . moveLeft
 moveUpperRight = moveUpper . moveRight
 moveBottomRight = moveBottom . moveRight
 
-aggregateAdjancents :: (Int, Int) -> [String] -> [Char]
-aggregateAdjancents xy vs =
+data Item = Item {xPos :: Int, yPos :: Int, itemChar :: Char} deriving (Show, Eq)
+
+aggregateAdjancents :: (Int, Int) -> [String] -> [Item]
+aggregateAdjancents xy@(x, y) vs =
   let (xl, yl) = matrixSize vs
    in map
-        (\f -> findItem (f xy) vs)
+        (\f -> Item x y (findItem (f xy) vs))
         ( case pos xy (xl - 1) (yl - 1) of
             UpperLeft -> [moveRight, moveBottom, moveBottomRight]
             Upper -> [moveLeft, moveRight, moveBottomRight, moveBottom, moveBottomLeft]
@@ -54,39 +64,95 @@ aggregateAdjancents xy vs =
 isRoleOfPaper :: (Int, Int) -> [String] -> Bool
 isRoleOfPaper pos xs = findItem pos xs == '@'
 
-countRoleOfPaper :: (Int, Int) -> [String] -> Int
-countRoleOfPaper pos vs = length $ filter (== '@') (aggregateAdjancents pos vs)
+isAccesable :: [Item] -> Bool
+isAccesable items = length (filter ((== '@') . itemChar) items) < 4
 
 solvePart1 :: [String] -> Int
 solvePart1 xs = go 0 (0, 0)
-  where
-    (xl, yl) = matrixSize xs
-    go acc (x, y)
-      | y == yl = acc
-      | x == xl = go acc (0, y + 1)
-      | otherwise = go (acc + if countRoleOfPaper (x, y) xs < 4 && isRoleOfPaper (x, y) xs then 1 else 0) (x + 1, y)
+ where
+  (xl, yl) = matrixSize xs
+  go acc (x, y)
+    | y == yl = acc
+    | x == xl = go acc (0, y + 1)
+    | otherwise = go (acc + if isRoleOfPaper (x, y) xs && isAccesable (aggregateAdjancents (x, y) xs) then 1 else 0) (x + 1, y)
 
 matrixSize :: [String] -> (Int, Int)
 matrixSize xs = (length $ head xs, length xs)
 
-example =
-    [ "..@@.@@@@."
-    , "@@@.@.@.@@"
-    , "@@@@@.@.@@"
-    , "@.@@@@..@."
-    , "@@.@@@@.@@"
-    , ".@@@@@@@.@"
-    , ".@.@.@.@@@"
-    , "@.@@@.@@@@"
-    , ".@@@@@@@@."
-    , "@.@.@@@.@."
-    ]
+removeRollOfPaper :: (Int, Int) -> [String] -> [String]
+removeRollOfPaper (x, y) orig =
+  zipWith
+    ( \i row ->
+        ( if i == y
+            then
+              take x row <> "." <> drop (x + 1) row
+            else
+              row
+        )
+    )
+    [0 .. length orig - 1]
+    orig
 
+removeRollOfPapers :: [Item] -> [String] -> [String]
+removeRollOfPapers [] vs = vs
+removeRollOfPapers ((Item{xPos, yPos}) : rest) vs = removeRollOfPapers rest (removeRollOfPaper (xPos, yPos) vs)
+
+scrape :: [String] -> [Item]
+scrape xs = go [] (0, 0)
+ where
+  (xl, yl) = matrixSize xs
+
+  go acc (x, y)
+    | y == yl = acc
+    | x == xl = go acc (0, y + 1)
+    | otherwise =
+        let items = aggregateAdjancents (x, y) xs
+            currentItem = Item x y '@'
+         in go (if isRoleOfPaper (x, y) xs && isAccesable (aggregateAdjancents (x, y) xs) then addUniquely currentItem acc else acc) (x + 1, y)
+
+solvePart2 :: [String] -> Int
+solvePart2  = go 0
+ where
+  go :: Int -> [String] -> Int
+  go acc xs =
+    let items = scrape xs
+        c = length items
+     in if c == 0 then acc else go (acc + c) (removeRollOfPapers items xs)
+
+example =
+  [ "..@@.@@@@."
+  , "@@@.@.@.@@"
+  , "@@@@@.@.@@"
+  , "@.@@@@..@."
+  , "@@.@@@@.@@"
+  , ".@@@@@@@.@"
+  , ".@.@.@.@@@"
+  , "@.@@@.@@@@"
+  , ".@@@@@@@@."
+  , "@.@.@@@.@."
+  ]
+
+example2 =
+  [ ".........."
+  , ".........."
+  , ".........."
+  , "....@@...."
+  , "...@@@@..."
+  , "...@@@@@.."
+  , "...@.@.@@."
+  , "...@@.@@@."
+  , "...@@@@@.."
+  , "....@@@..."
+  ]
 readInput :: String -> IO [String]
 readInput f = lines <$> readFile f
 
 runExamplePart1 = solvePart1 example
+runExamplePart2 = solvePart2 example
 runPart1 = solvePart1 <$> readInput "04.input"
+runPart2 = solvePart2 <$> readInput "04.input"
 
 main :: IO ()
-main =  runPart1 >>= print
+main = do
+  -- print runExamplePart2
+  runPart2 >>= print
